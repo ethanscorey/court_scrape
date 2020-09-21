7e117254d08bf1a5a28b8fce893bf5a70c25b174
Ethan Corey <ethanscorey@gmail.com>
2020-09-21 11:12:58 -0400

    Delete chunk.py

--- chunk.py
+++
@@ -1,7 +0,0 @@
-def chunk_string(string, chunk_size):
-    str_len = len(string)
-    if str_len % chunk_size == 0:
-        n_chunks = str_len // chunk_size
-    else:
-        n_chunks = str_len // chunk_size + 1
-    return (string[i*chunk_size:(i+1)*chunk_size] for i in range(n_chunks))
